4a7e73823d209e3958b19986a0751b980bfb7d00
Fuxian Qin <qinfuxian2014@outlook.com>
2022-12-12 15:55:27 +0800

    Create waitall.js

---
+++ waitall.js
@@ -0,0 +1,45 @@
+    function waitForElm(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    resolve(document.querySelector(selector));
+                    observer.disconnect();
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
+    
+    async function waitAndClick(selector) {
+        const el = await waitForElm(selector);
+        if (el) {el.click();}
+    }
+    
+    async function waitAndFocus(selector) {
+        const el = await waitForElm(selector);
+        if (el) {el.focus();}
+    }
+    
+    async function waitFocusWithVal(selector, value) {
+        const el = await waitForElm(selector);
+        if (el) {
+            el.focus();
+            el.value = value
+        }
+    }
+    
+    async function waitClickWithVal(selector, value) {
+        const el = await waitForElm(selector);
+        if (el) {
+            el.click();
+            el.value = value
+        }
+    }
